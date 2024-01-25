45da41f6dcb781e6c27a19d6d7da8527c18a28c1
munseopyun <ung5@naver.com>
2024-01-25 21:47:11 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Customer from './compoents/Customer';
+import { Paper } from '@mui/material';
+import { styled } from '@mui/system';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
 
+const StyledPaper = styled(Paper)({
+  width: '100%',
+  marginTop: 16, // 이것은 theme.spacing.unit * 3을 대체합니다.
+  overflowX: 'auto',
+});
+
+const StyledTable = styled(Table)({
+  minWidth: 1080,
+});
 
 const customers = [{
   'id':1,
@@ -28,23 +45,36 @@ const customers = [{
   'job':'디자이너',
 }]
 
-function App() {
+const App = () => {
   return (
-    <div>
-      {customers.map(c => (
-        <Customer
-          key={c.id}
-          id={c.id}
-          image={c.image}
-          name={c.name}
-          birthday={c.birthday}
-          gender={c.gender}
-          job={c.job}
-        />
-      ))}
-    </div>
+    <StyledPaper>
+      <StyledTable>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {customers.map(c => (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          ))}
+        </TableBody>
+      </StyledTable>
+    </StyledPaper>
   );
 }
-
 
 export default App;
