7e243cb9bbacf445167eab7a16d1689df4b34dab
munseopyun <ung5@naver.com>
2024-01-30 00:18:12 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Customer from 'C:/Users/2022-PC(T)-27/Desktop/SKT FLY AI 4기/Study/mini_project_na/management/client/src/compoents/Customer';
-import { Paper } from '@mui/material';
+import { Paper, CircularProgress } from '@mui/material'; 
 import { styled } from '@mui/system';
 import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
@@ -16,12 +16,21 @@ const StyledPaper = styled(Paper)({
   overflowX: 'auto',
 });
 
-const StyledTable = styled(Table)({
+const StyledTable = styled(Table)(({ theme }) => ({
   minWidth: 1080,
-});
+  progress: {
+    margin: 'auto', // Center the CircularProgress horizontally and vertically
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: theme.spacing(2), // Add top margin if needed
+  },
+}));
 
 const App = () => {
   const [customers, setCustomers] = useState([]);
+  const [completed, setCompleted] = useState(0);
+  let timer;
 
   useEffect(() => {
     const callApi = async () => {
@@ -29,12 +38,24 @@ const App = () => {
         const response = await fetch('/api/customers');
         const data = await response.json();
         setCustomers(data);
+        setCompleted(1); // Set completed to 1 when data fetching is complete
       } catch (error) {
         console.error('Error fetching data:', error);
+        setCompleted(-1); // Set completed to -1 in case of an error
       }
     };
 
+    const progress = () => {
+      setCompleted((prevCompleted) => (prevCompleted >= 100 ? 0 : prevCompleted + 1));
+    };
+
+    timer = setInterval(progress, 10);
     callApi();
+
+    // Clean up the interval when the component unmounts
+    return () => {
+      clearInterval(timer);
+    };
   }, []);
 
   return (
@@ -51,7 +72,7 @@ const App = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {customers.map(c => (
+          {customers.map((c) => (
             <Customer
               key={c.id}
               id={c.id}
@@ -65,12 +86,21 @@ const App = () => {
           {customers.length === 0 && (
             <TableRow>
               <TableCell colSpan={6}>데이터가 없습니다.</TableCell>
+              <TableCell colSpan={6} align='center'>
+              <CircularProgress
+                    className={StyledTable.progress}
+                    variant="determinate"
+                    value={completed}
+                    size={40} // 크기 조절
+                    thickness={5} // 두께 조절
+                  />
+              </TableCell>
             </TableRow>
           )}
         </TableBody>
       </StyledTable>
     </StyledPaper>
   );
-}
+};
 
 export default App;
